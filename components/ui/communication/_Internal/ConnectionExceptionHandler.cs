9b68ce524f3fd225f18ede6b340e6b1398466bfb
Matthew van der Hoorn <md.registrations@protonmail.com>
2024-08-24 20:53:10 +0200

    Added Connection Exception Handler

---
+++ components/ui/communication/_Internal/ConnectionExceptionHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+
+namespace MD.RPM.UI.Communication._Internal;
+
+/// <summary>
+/// Handles any exceptions that occur during communication with the server.
+/// </summary>
+public class ConnectionExceptionHandler
+{
+    public ConnectionExceptionHandler()
+    {
+        
+    }
+
+    private void LogException(Exception ex, string methodName, string description)
+    {
+        Console.WriteLine($"{description} | Method: {methodName} | Message: {ex.Message}");
+    }
+
+    public void HandleException(Exception ex, [CallerMemberName] string? methodName = null)
+    {
+        switch (ex)
+        {
+            case IOException:
+                LogException(ex, methodName!, "An IO Error occurred while communicating with the server.");
+                break;
+            case HttpRequestException:
+                LogException(ex, methodName!, "An HTTP request error occurred while communicating with the server.");
+                break;
+            case WebException:
+                LogException(ex, methodName!, "A web error occurred while communicating with the server.");
+                break;
+            case SocketException:
+                LogException(ex, methodName!, "A socket error occurred while communicating with the server.");
+                break;
+        }
+    }
+}
